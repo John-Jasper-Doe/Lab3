530f3d37656f2690c845a4dc80c40e477b4749e9
John 'jasper' Doe <john.jasper.doe@gmail.com>
2019-04-18 01:06:52 +0300

    Fixed class 	переименовано:              src/chunklist.h.autosave -> src/chunklist.h

--- src/chunklist.h
+++ src/chunklist.h
@@ -25,14 +25,21 @@ class chunk_list {
   public:
     chunk_list() {
     #ifdef DEBUGING
-      std::cout << "CAPACITY = " << CAPACITY <<
-                   "Type size = " << sizeof(T) << std::endl;
+      std::cout << "CAPACITY = " << CAPACITY << "\n" <<
+                   "Type size = " << sizeof(int) << " byte" << std::endl;
     #endif
 
       for (std::size_t i = 0; i < CAPACITY; ++i) {
-        start_list_[i].next = &start_list_[i + 1]
-      }
+        if (i == 0)
+          ptr_list_[i].prev = nullptr;
+        else
+          ptr_list_[i]. prev = &ptr_list_[i - 1];
 
+        if (i == CAPACITY - 1)
+          ptr_list_[i].next = nullptr;
+        else
+          ptr_list_[i].next = &ptr_list_[i + 1];
+      }
     }
 
     ~chunk_list() {
@@ -41,11 +48,12 @@ class chunk_list {
 
     /* moved */
     chunk_list(chunk_list &&other) noexcept {
-
+      (void)other;
     }
 
     chunk_list & operator =(chunk_list &&other) noexcept {
-
+      (void)other;
+      return *this;
     }
 
     /* don't copyed */
@@ -54,10 +62,9 @@ class chunk_list {
 
   private:
     std::size_t size_ = 0;
-
-    chunk<T> *start_list_ = static_cast<chunk<T> *>(::operator new[](CAPACITY *
-                                                    sizeof(chunk<T>)));
-    chunk<T> *head_ = start_list_;
+    chunk<T> *ptr_list_ =
+        static_cast<chunk<T> *>(::operator new[](CAPACITY * sizeof(chunk<T>)));
+    chunk<T> *head_ = ptr_list_;
 };
 
 #endif /* CHUNKLIST_H_ */
